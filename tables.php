5021fbec3f022defb0f1b8c68e875a23821fee48
mikespub <github@mikespub.net>
2025-10-19 18:38:34 +0200

    add tables

---
+++ tables.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Xaraya\Modules\Hitcount;
+
+use xarDB;
+
+class Tables
+{
+    /**
+     * Hitcount Module
+     *
+     * @package modules
+     * @subpackage hitcount module
+     * @category Third Party Xaraya Module
+     * @version 2.0.0
+     * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+     * @link http://xaraya.com/index.php/release/177.html
+     * @author Hitcount Module Development Team
+     */
+    public function __invoke(?string $prefix = null)
+    {
+        // Initialise table array
+        $xartable = [];
+        $prefix ??= xarDB::getPrefix();
+
+        // Name for hitcount database entities
+        $hitcount = $prefix . '_hitcount';
+
+        // Table name
+        $xartable['hitcount'] = $hitcount;
+
+        // Return table information
+        return $xartable;
+    }
+}
